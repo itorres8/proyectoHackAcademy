16d78681737f5ccaa111b14077113c78c2e05e17
Nacho <ignaciotorresmayobre8@gmail.com>
2024-11-19 22:56:20 -0300

    Co-authored-by: Christian Vera <verachristianfder@gmail.com> Co-authored-by: Nicolás Acosta <nicoAcoshub@users.noreply.github.com> Co-authored-by: willeliwood <willeliwood@users.noreply.github.com>

---
+++ src/components/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import NavScrollExample from './Navbar'
+
+const Layout = () => {
+  return (
+    <div>
+        <header>
+            <NavScrollExample />            
+        </header>
+        <main>
+      <Outlet />
+      </main>
+      <footer>
+        <p>Hack Academy Copyright <i className="bi bi-c-circle"></i></p>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavScrollExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Hack Plus</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Inicio</Nav.Link>
+            <Nav.Link href="#action2"></Nav.Link>
+            <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Buscar"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="success"><i className="bi bi-search"></i></Button>
+            
+          </Form>
+            <NavDropdown title="Perfil" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Suscripción</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Perfil
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Cerrar Sesión
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavScrollExample;
