ebcce19f09d59b3025087846fe98271fde448034
nicoAcoshub <nicolas.acosta@student.ha.dev>
2024-11-20 22:06:34 -0300

    Co-authored-by: Christian Vera <verachristianfder@gmail.com> Co-authored-by: willeliwood <willeliwood@users.noreply.github.com>

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,45 +1,50 @@
-import React from 'react'
-/* import { login } from '../Api/movieDb' */
+import React from "react";
 
 const Login = () => {
-
-/*   try {
-    login({
-      email: "jperez@gmail.com",
-      password: "1234"
-    })
-  
-  } catch (error) {
-    console.log(error)
-  }
- */
-
   return (
     <div className="raw justify-content-center">
-        <div className="col-4 mx-auto mb-3 ">
-      <i className="bi bi-0-circle"></i>
-      
-      <div className="card p-4 bg-secondary">
-        <h2>Escribir correo para continuar</h2>
-        <p>Es necesario iniciar sesión con tu cuenta de HackPlus. En caso de no tener una recibirás indicaciones para crearla.</p>
-        <form onSubmit="">
-          <label htmlFor="email">Correo:</label>
-        <input type="email" placeholder="Correo electrónico" name="email" defaultValue="" required/>
-        <button type="submit">Continuar</button>
-        </form>
+      <div className="col-4 mx-auto mb-3 ">
+        <i className="bi bi-0-circle"></i>
+
+        <div className="card p-4 bg-secondary">
+          <h2>Escribir correo para continuar</h2>
+          <p>
+            Es necesario iniciar sesión con tu cuenta de HackPlus. En caso de no
+            tener una recibirás indicaciones para crearla.
+          </p>
+          <form onSubmit="">
+            <label htmlFor="email">Correo:</label>
+            <input
+              type="email"
+              placeholder="Correo electrónico"
+              name="email"
+              defaultValue=""
+              required
+            />
+            <button type="submit">Continuar</button>
+          </form>
         </div>
       </div>
       <div className="col-4 mx-auto card p-4 bg-secondary">
         <h2>Escribir contraseña</h2>
-        <p>Es necesario iniciar sesión con tu cuenta de HackPlus con tu correo electrónico: correo@ejemplo.com</p>
+        <p>
+          Es necesario iniciar sesión con tu cuenta de HackPlus con tu correo
+          electrónico: correo@ejemplo.com
+        </p>
         <form onSubmit="">
           <label htmlFor="password">Contraseña</label>
-        <input type="password" placeholder="Contraseña.." name="password" defaultValue="" required/>
-        <button type="submit">Iniciar sesión</button>
+          <input
+            type="password"
+            placeholder="Contraseña.."
+            name="password"
+            defaultValue=""
+            required
+          />
+          <button type="submit">Iniciar sesión</button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
