e8156232eca3d76c5c1131b7f4e19ab8e6cb3789
nicoAcoshub <nicolas.acosta@student.ha.dev>
2024-11-21 18:43:48 -0300

    prueba

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-const Movie = () 
+const Movie = () +
 
 const Movie = () => {
   return (
     <div>
-      
+      <p></p>
     </div>
   )
 }
