af65da0b8d683e0e9f40cd085c79fbc693a3d15f
nicoAcoshub <nicolas.acosta@student.ha.dev>
2024-12-05 11:36:48 -0300

    Correcciones para build

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import styles from "../styles/login-register.module.css";
+import styles from "../styles/Login-register.module.css";
 
 const Login = () => {
   const dispatch = useDispatch();
